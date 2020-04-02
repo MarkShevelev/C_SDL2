63c5bfc5ea551f5dbcc8bda78e0f9ddb8e554671
Mark Shevelev <lionishy@gmail.com>
2020-04-02 12:13:23 +0300

    Простейшая программа с использованием ресурсов

---
+++ 03_SimplestProgramWithResources.c
@@ -0,0 +1,25 @@
+#include <SDL.h>
+#include <stdio.h>
+
+int main(int arc, char **argv) {
+    if (0 != SDL_Init(SDL_INIT_VIDEO)) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2 Demo",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+        );
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
